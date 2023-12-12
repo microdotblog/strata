5ebd1acd3db0d70cb9746c7ae448f2b65e40c30d
Vincent Ritter <git@vincentritter.com>
2023-12-12 12:16:10 +0100

    Add string checker in prep for login.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import {AppRegistry} from 'react-native';
 import MainApp from './screens/App';
 import {name as appName} from './../app.json';
 import './utils/snapshots';
+import './utils/string_checker';
 
 AppRegistry.registerComponent(appName, () => MainApp);
