236b9fe88d08ba4d52241e7e2de7ba5b2e02f617
Vincent Ritter <git@vincentritter.com>
2023-12-15 12:21:40 +0100

    Tweak padding and font size.

--- src/components/sheets/login_message.js
+++ src/components/sheets/login_message.js
@@ -1,7 +1,7 @@
 import ActionSheet from "react-native-actions-sheet";
 import * as React from 'react';
 import { observer } from 'mobx-react';
-import { View, Text } from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 import Login from './../../stores/Login';
 
 @observer
@@ -11,16 +11,16 @@ export default class LoginMessageSheet extends React.Component {
       <ActionSheet
         containerStyle={{
           backgroundColor: '#6EE7B7',
-          padding: 8,
+          padding: 15,
           paddingHorizontal: 12,
           borderRadius: 5,
           elevation: 2
         }}
         id={this.props.sheet_id}
       >
-        <View style={{ justifyContent: "center", alignItems: "center" }}>
-          <Text style={{ textAlign: "center", color: "#064E3B", fontWeight: "600" }}>{Login.message}</Text>
-        </View>
+        <SafeAreaView style={{ justifyContent: "center", alignItems: "center" }}>
+          <Text style={{ textAlign: "center", color: "#064E3B", fontWeight: "600", fontSize: 17, paddingVertical: 15 }}>{Login.message}</Text>
+        </SafeAreaView>
       </ActionSheet>
     )
   }
