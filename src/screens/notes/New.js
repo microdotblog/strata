184089bd95a7c37fd38a67f480d9eb65e9947295
Vincent Ritter <git@vincentritter.com>
2023-12-17 18:10:50 +0100

    Prep for showing basic New and Edit text inputs.

--- src/screens/notes/New.js
+++ src/screens/notes/New.js
@@ -1,17 +1,43 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
-import { View, Text } from 'react-native';
+import { TextInput, KeyboardAvoidingView } from 'react-native';
 // import App from '../../stores/App'
 // import Auth from '../../stores/Auth'
 
 @observer
 export default class NewNoteModalScreen extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.input_accessory_view_id = "input_toolbar";
+  }
+
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <Text>New note</Text>
-      </View>
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined} style={{ flex: 1, backgroundColor: App.theme_background_color() }}>
+        <TextInput
+          placeholderTextColor="lightgrey"
+          style={{
+            fontSize: 18,
+            justifyContent: 'flex-start',
+            alignItems: 'flex-start',
+            marginTop: 3,
+            marginBottom: 38,
+            padding: 8,
+            color: App.theme_text_color()
+          }}
+          multiline={true}
+          scrollEnabled={true}
+          returnKeyType={'default'}
+          keyboardType={'default'}
+          autoFocus={true}
+          autoCorrect={true}
+          clearButtonMode={'while-editing'}
+          enablesReturnKeyAutomatically={true}
+          underlineColorAndroid={'transparent'}
+          inputAccessoryViewID={this.input_accessory_view_id}
+        />
+      </KeyboardAvoidingView>
     )
   }
 
