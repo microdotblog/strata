5e5847d14f3edf48062b8891a9d14cc4222765e3
Vincent Ritter <git@vincentritter.com>
2024-01-13 15:43:50 +0100

    Tidy up swipe action view.

--- src/components/notes/note_item.js
+++ src/components/notes/note_item.js
@@ -21,17 +21,20 @@ export default class NoteItem extends React.Component {
         {
           this._return_action(
             'Delete',
-            "rgb(239,68,68)",
+            App.theme_danger_color(),
             60,
             progress,
-            `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2"><path d="M 21 2 C 19.354545 2 18 3.3545455 18 5 L 18 7 L 10.154297 7 A 1.0001 1.0001 0 0 0 9.984375 6.9863281 A 1.0001 1.0001 0 0 0 9.8398438 7 L 8 7 A 1.0001 1.0001 0 1 0 8 9 L 9 9 L 9 45 C 9 46.645455 10.354545 48 12 48 L 38 48 C 39.645455 48 41 46.645455 41 45 L 41 9 L 42 9 A 1.0001 1.0001 0 1 0 42 7 L 40.167969 7 A 1.0001 1.0001 0 0 0 39.841797 7 L 32 7 L 32 5 C 32 3.3545455 30.645455 2 29 2 L 21 2 z M 21 4 L 29 4 C 29.554545 4 30 4.4454545 30 5 L 30 7 L 20 7 L 20 5 C 20 4.4454545 20.445455 4 21 4 z M 11 9 L 18.832031 9 A 1.0001 1.0001 0 0 0 19.158203 9 L 30.832031 9 A 1.0001 1.0001 0 0 0 31.158203 9 L 39 9 L 39 45 C 39 45.554545 38.554545 46 38 46 L 12 46 C 11.445455 46 11 45.554545 11 45 L 11 9 z M 18.984375 13.986328 A 1.0001 1.0001 0 0 0 18 15 L 18 40 A 1.0001 1.0001 0 1 0 20 40 L 20 15 A 1.0001 1.0001 0 0 0 18.984375 13.986328 z M 24.984375 13.986328 A 1.0001 1.0001 0 0 0 24 15 L 24 40 A 1.0001 1.0001 0 1 0 26 40 L 26 15 A 1.0001 1.0001 0 0 0 24.984375 13.986328 z M 30.984375 13.986328 A 1.0001 1.0001 0 0 0 30 15 L 30 40 A 1.0001 1.0001 0 1 0 32 40 L 32 15 A 1.0001 1.0001 0 0 0 30.984375 13.986328 z"/></svg>`
+            `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+            </svg>`,
+            'trash'
           )
         }
       </View>
     </View>
   )
 
-  _return_action = (text, color, x, progress, icon) => {
+  _return_action = (text, color, x, progress, icon, symbol) => {
 
     const trans = progress.interpolate({
       inputRange: [0, 1],
@@ -62,12 +65,24 @@ export default class NoteItem extends React.Component {
             borderRadius: 12
           }}
           onPress={press_handler}>
-          <SvgXml
-            xml={icon}
-            width={22}
-            height={22}
-            stroke={color}
-          />
+          {
+            Platform.OS === 'ios' ?
+              <SFSymbol
+                name={symbol}
+                color={color}
+                style={{ height: 22, width: 22 }}
+                multicolor={true}
+              />
+              :
+              <SvgXml
+                style={{
+                  height: 22,
+                  width: 22
+                }}
+                color={color}
+                xml={icon}
+              />
+          }
         </RectButton>
       </Animated.View>
     );
