2c77af6addb74001a4ca409de3a9a8c3c90748c9
Vincent Ritter <git@vincentritter.com>
2024-02-05 16:57:20 +0100

    Upgrade to RN0.73.3.

--- src/utils/dev.js
+++ src/utils/dev.js
@@ -3,5 +3,6 @@ import { LogBox } from 'react-native';
 LogBox.ignoreLogs([
   "Require cycle:",
   "Sending `appearanceChanged` with no listeners registered",// Seems to be new in 17.2
-  "Sending `onAnimatedValueUpdate` with no listeners registered"
+  "Sending `onAnimatedValueUpdate` with no listeners registered",
+  "RCTBridge required dispatch_sync to load RCTAccessibilityManager"
 ])
