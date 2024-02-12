4a10e739b906f7b8b2e2a5d63142a6dbdba03d8b
Manton Reece <manton@me.com>
2024-02-12 14:24:22 -0600

    Started rearranging for iOS-specific text view.

--- src/screens/notes/Edit.js
+++ src/screens/notes/Edit.js
@@ -4,6 +4,7 @@ import { TextInput, KeyboardAvoidingView, InputAccessoryView, ActivityIndicator,
 import Posting from '../../stores/Posting'
 import PostingToolbar from '../../components/keyboard/posting_toolbar';
 import App from '../../stores/App'
+import HighlightingText from '../../components/text/highlighting_text';
 // import Auth from '../../stores/Auth'
 
 @observer
@@ -19,66 +20,73 @@ export default class EditNoteModalScreen extends React.Component {
   }
 
   render() {
-    return (
-      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined} style={{ flex: 1, backgroundColor: App.theme_background_color() }}>
-        <TextInput
-          placeholderTextColor="lightgrey"
-          style={{
-            fontSize: 17,
-            justifyContent: 'flex-start',
-            alignItems: 'flex-start',
-            marginTop: 5,
-            marginBottom: 90,
-            padding: 10,
-            color: App.theme_text_color()
-          }}
-          multiline={true}
-          scrollEnabled={true}
-          returnKeyType={'default'}
-          keyboardType={'default'}
-          autoFocus={true}
-          autoCorrect={true}
-          clearButtonMode={'while-editing'}
-          enablesReturnKeyAutomatically={true}
-          underlineColorAndroid={'transparent'}
-          inputAccessoryViewID={this.input_accessory_view_id}
-          value={Posting.note_text}
-          selection={Posting.text_selection}
-          onChangeText={(text) => !Posting.is_sending_note ? Posting.set_note_text(text) : null}
-          onSelectionChange={({ nativeEvent: { selection } }) => {
-            Posting.set_text_selection(selection)
-          }}
-          editable={!Posting.is_sending_note}
-        />
-        {
-          Platform.OS === 'ios' ?
-            <InputAccessoryView nativeID={this.input_accessory_view_id}>
-              <PostingToolbar />
-            </InputAccessoryView>
-            :
-            <>
-              <PostingToolbar />
-            </>
-        }
-        {
-          Posting.is_sending_note ?
-            <View
-              style={{
-                position: 'absolute',
-                top: 0,
-                height: 200,
-                width: '100%',
-                justifyContent: 'center',
-                alignItems: 'center',
-                zIndex: 10
-              }}
-            >
-              <ActivityIndicator color={App.theme_accent_color()} size={'large'} />
-            </View>
-            : null
-        }
-      </KeyboardAvoidingView>
-    )
+    if (Platform.OS === 'ios') {
+      return (
+        <TextInput />
+      )
+    }
+    else {
+      return (
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined} style={{ flex: 1, backgroundColor: App.theme_background_color() }}>
+          <TextInput
+            placeholderTextColor="lightgrey"
+            style={{
+              fontSize: 17,
+              justifyContent: 'flex-start',
+              alignItems: 'flex-start',
+              marginTop: 5,
+              marginBottom: 90,
+              padding: 10,
+              color: App.theme_text_color()
+            }}
+            multiline={true}
+            scrollEnabled={true}
+            returnKeyType={'default'}
+            keyboardType={'default'}
+            autoFocus={true}
+            autoCorrect={true}
+            clearButtonMode={'while-editing'}
+            enablesReturnKeyAutomatically={true}
+            underlineColorAndroid={'transparent'}
+            inputAccessoryViewID={this.input_accessory_view_id}
+            value={Posting.note_text}
+            selection={Posting.text_selection}
+            onChangeText={(text) => !Posting.is_sending_note ? Posting.set_note_text(text) : null}
+            onSelectionChange={({ nativeEvent: { selection } }) => {
+              Posting.set_text_selection(selection)
+            }}
+            editable={!Posting.is_sending_note}
+          />
+          {
+            Platform.OS === 'ios' ?
+              <InputAccessoryView nativeID={this.input_accessory_view_id}>
+                <PostingToolbar />
+              </InputAccessoryView>
+              :
+              <>
+                <PostingToolbar />
+              </>
+          }
+          {
+            Posting.is_sending_note ?
+              <View
+                style={{
+                  position: 'absolute',
+                  top: 0,
+                  height: 200,
+                  width: '100%',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  zIndex: 10
+                }}
+              >
+                <ActivityIndicator color={App.theme_accent_color()} size={'large'} />
+              </View>
+              : null
+          }
+        </KeyboardAvoidingView>
+      )
+    }
   }
 
 }
