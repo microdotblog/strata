fddd0165204c8c875d6e3d30d50d5966fcb85105
Vincent Ritter <git@vincentritter.com>
2026-05-31 13:56:48 +0200

    Revert Vision Camera v5 upgrade and restore the v4 QR scanner implementation.

--- src/components/sheets/_code_scanner.js
+++ src/components/sheets/_code_scanner.js
@@ -1,42 +1,30 @@
-import {View} from 'react-native';
-import {useCameraDevice, useCameraPermission} from 'react-native-vision-camera';
-import {CodeScanner} from 'react-native-vision-camera-barcode-scanner';
+import { useCodeScanner, useCameraPermission, useCameraDevice, Camera } from 'react-native-vision-camera'
+import { View } from 'react-native'
 
-export default function QRScanner({onCodeScanned}) {
-  const device = useCameraDevice('back');
-  const {hasPermission} = useCameraPermission();
-
-  if (device != null && hasPermission) {
-    return (
-      <View
-        style={{
-          backgroundColor: 'black',
-          marginBottom: 12,
-          height: 370,
-          width: '100%',
-          borderRadius: 8,
-          overflow: 'hidden',
-        }}>
-        <CodeScanner
-          isActive={true}
-          style={{flex: 1, borderRadius: 8}}
-          barcodeFormats={['qr-code']}
-          onBarcodeScanned={onCodeScanned}
-        />
+export default function QRScanner({ onCodeScanned }) {
+  
+  const device = useCameraDevice('back')
+  const { hasPermission } = useCameraPermission()
+  
+  const codeScanner = useCodeScanner({
+    codeTypes: ["qr"],
+    onCodeScanned: (codes) => {
+      onCodeScanned(codes)
+    }
+  })
+  
+  if(device != null && hasPermission){
+    return(
+      <View style={{ backgroundColor: 'black', marginBottom: 12, height: 370, width: "100%", borderRadius: 8, overflow: 'hidden' }}>
+        <Camera isActive={true} device={device} style={{ flex: 1, borderRadius: 8 }} codeScanner={codeScanner} />
       </View>
-    );
-  } else if (device == null && __DEV__) {
-    return (
-      <View
-        style={{
-          backgroundColor: 'black',
-          marginBottom: 12,
-          height: 370,
-          width: '100%',
-          borderRadius: 8,
-        }}
-      />
-    );
+    ) 
   }
-  return null;
+  else if(device == null && __DEV__){
+    return(
+      <View style={{ backgroundColor: 'black', marginBottom: 12, height: 370, width: "100%", borderRadius: 8 }} />
+    )
+  }
+  return null
+  
 }
--- src/components/sheets/_secret_key.js
+++ src/components/sheets/_secret_key.js
@@ -6,49 +6,32 @@ import Auth from '../../stores/Auth'
 import Tokens from "../../stores/Tokens";
 import { SvgXml } from 'react-native-svg';
 import QRScanner from './_code_scanner';
-import {
-  VisionCamera,
-  getAllCameraDevices,
-  addOnCameraDevicesChangedListener,
-} from 'react-native-vision-camera';
+import { Camera } from 'react-native-vision-camera';
 
 @observer
 export default class SecretKeyInput extends React.Component {
   
   state = {
     isScanning: false,
-    canScan: this.hasCameraHardware(),
-  };
-
-  cameraDevicesListener = null;
-
-  hasCameraHardware() {
-    return __DEV__ || getAllCameraDevices().length > 0 || Platform.OS === 'android';
-  }
-
-  componentDidMount() {
-    const cameraPermission = VisionCamera.cameraPermissionStatus;
-    if (cameraPermission === 'denied' || cameraPermission === 'restricted') {
-      this.setState({canScan: false});
-    }
-
-    this.cameraDevicesListener = addOnCameraDevicesChangedListener(devices => {
-      if (devices.length > 0 && !this.state.canScan) {
-        this.setState({canScan: true});
-      }
-    });
+    canScan: __DEV__ || Camera.getAvailableCameraDevices()?.length > 0
   }
   
-  componentWillUnmount() {
-    this.cameraDevicesListener?.remove();
-    if (this.state.isScanning) {
-      this.setState({isScanning: false});
+  componentDidMount(){
+    const cameraPermission = Camera.getCameraPermissionStatus()
+    if((cameraPermission == "denied" || cameraPermission == "restricted") && Platform.OS == "ios"){
+      this.setState({ canScan: false })
+    }
+  }
+  
+  componentWillUnmount(){
+    if(this.state.isScanning){
+      this.setState({ isScanning: false })
     }
   }
   
   handleCodeScanned = (codes) => {
-    if (codes.length > 0 && codes[0].rawValue) {
-      const codeValue = codes[0].rawValue;
+    if (codes.length > 0 && codes[0].value) {
+      const codeValue = codes[0].value
       if(codeValue?.includes("strata://qrcode/")){
         Tokens.set_temp_secret_token(codeValue.replace("strata://qrcode/", ""))
         this.setState({ isScanning: false })
@@ -57,16 +40,17 @@ export default class SecretKeyInput extends React.Component {
   }
   
   toggleScanner = async () => {
-    const cameraPermission = VisionCamera.cameraPermissionStatus;
-    if (cameraPermission === 'authorized') {
-      this.setState(prevState => ({isScanning: !prevState.isScanning}));
-    } else {
-      const granted = await VisionCamera.requestCameraPermission();
-      if (granted) {
-        this.setState(prevState => ({isScanning: !prevState.isScanning}));
+    const cameraPermission = Camera.getCameraPermissionStatus()
+    if(cameraPermission == "granted"){
+      this.setState(prevState => ({ isScanning: !prevState.isScanning }))
+    }
+    else{
+      const permission = await Camera.requestCameraPermission()
+      if(permission == "granted"){
+        this.setState(prevState => ({ isScanning: !prevState.isScanning }))
       }
     }
-  };
+  }
 
   render() {
     
@@ -145,7 +129,7 @@ export default class SecretKeyInput extends React.Component {
             }}
           >
             {
-              this.state.canScan ?
+              this.state.canScan && Platform.OS === 'ios' ?
               <TouchableOpacity
                 onPress={this.toggleScanner}
                 style={{
